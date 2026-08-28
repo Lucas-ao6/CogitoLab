ec22d70d0959fa383c11b65a76be727be696d479
Lucas-ao6 <lucas.oliveira35@estudante.ufla.br>
2026-08-27 22:07:44 -0300

    teste dnv pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://lucas-ao6.github.io/CogitoLab',
-  base: '/CogitoLab',
+    site: 'https://lucas-ao6.github.io',
+    base: '/CogitoLab',
 });
