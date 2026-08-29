3b8db9ff0a81fe5be15ae7df6d4c93758c24149b
Lucas-ao6 <lucas.oliveira35@estudante.ufla.br>
2026-08-29 15:25:17 -0300

    mandei errado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-// // @ts-check
-// import { defineConfig } from 'astro/config';
-
-// export default defineConfig({
-//   site: 'https://lucas-ao6.github.io',
-//   base: '/CogitoLab',
-// });
-
+// @ts-check
 import { defineConfig } from 'astro/config';
-export default defineConfig({});
+
+export default defineConfig({
+  site: 'https://lucas-ao6.github.io',
+  base: '/CogitoLab',
+});
+
+// PRA RODAR NO LOCAL
+// import { defineConfig } from 'astro/config';
+// export default defineConfig({});
